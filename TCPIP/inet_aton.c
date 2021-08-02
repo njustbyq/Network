0c7a4b3c59b6d5b588f76f5fd96324e90e0d4359
njustbyq <2042675095@qq.com>
2021-08-02 17:07:14 +0800

    address init

---
+++ TCPIP/inet_aton.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+void error_hadling(char *message);
+
+int main()
+{
+    char *addr = "127.232.124.79";
+    struct sockaddr_in addr_inet;
+    
+    if(!inet_aton(addr, &addr_inet.sin_addr))
+    {
+        error_hadling("Conversion error");
+    }
+    else
+    {
+        printf("Network ordered integer addr: %#x \n", addr_inet.sin_addr.s_addr);
+    }
+    return 0;
+}
+
+void error_hadling(char *message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
